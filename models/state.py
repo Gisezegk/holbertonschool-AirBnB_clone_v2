d60d27b6105b45a5a915229169c20857864d811f
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-03-27 04:06:42 +0000

    6DBS

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            c_instances = storage.all(City).values()
+            m_cities = []
+            for i in c_instances:
+                if i.state_id == self.id:
+                    m_cities.append(i)
+            return m_cities
