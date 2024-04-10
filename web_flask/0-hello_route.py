288d45a29891056e77e6e528af59f702d022bdc1
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-04-10 20:25:41 +0000

    web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!bin/usr/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host="0.0.0.0", port=5000)
